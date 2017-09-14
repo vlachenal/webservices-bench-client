00d1fad8fa18bc6aa0bd119fc671aa7b1106fda6
Vincent Lachenal <vincent.lachenal@gmail.com>
2017-09-14 16:05:46 +0200

    Implements mapper choice

---
+++ src/soap.generated/java/com/github/vlachenal/webservice/bench/client/soap/api/Mapper.java
@@ -0,0 +1,47 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7-b41 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2017.09.12 à 11:14:30 AM CEST 
+//
+
+
+package com.github.vlachenal.webservice.bench.client.soap.api;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour mapper.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="mapper">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="MANUAL"/>
+ *     &lt;enumeration value="DOZER"/>
+ *     &lt;enumeration value="MAPSTRUCT"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "mapper")
+@XmlEnum
+public enum Mapper {
+
+    MANUAL,
+    DOZER,
+    MAPSTRUCT;
+
+    public String value() {
+        return name();
+    }
+
+    public static Mapper fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
