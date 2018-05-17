c888c0159f21faed8450b3a570a7c7238b2c9599
Vincent Lachenal <vincent.lachenal@gmail.com>
2018-05-17 23:46:46 +0200

    Publish new result using Lock and Condition instead of Semaphore. Make deleteAll synchronous.

--- src/main/java/com/github/vlachenal/webservice/bench/client/webflux/api/RESTWebfluxClient.java
+++ src/main/java/com/github/vlachenal/webservice/bench/client/webflux/api/RESTWebfluxClient.java
@@ -7,7 +7,9 @@
 package com.github.vlachenal.webservice.bench.client.webflux.api;
 
 import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
@@ -88,9 +90,18 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
    */
   @Override
   public void deleteAll() {
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+    lock.lock();
     client.mutate().build().delete().exchange().doOnError(e -> {
       LOG.error("Error while deleting customers: " + e.getMessage(), e);
+    }).doFinally(t -> {
+      lock.lock();
+      cond.signal();
+      lock.unlock();
     }).subscribe();
+    cond.awaitUninterruptibly();
+    lock.unlock();
   }
 
   /**
@@ -100,11 +111,13 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
    * @param call the call
    * @param mutex the mutex to unlock
    */
-  private void manageClientError(final Throwable error, final ClientCall call, final Semaphore mutex) {
+  private void manageClientError(final Throwable error, final ClientCall call, final Lock lock, final Condition cond) {
+    lock.lock();
     call.setOk(false);
     call.setErrMsg(error.getMessage());
     call.setClientEnd(System.nanoTime());
-    mutex.release();
+    cond.signal();
+    lock.unlock();
   }
 
   /**
@@ -115,10 +128,10 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
    * @param onSuccess the on success consumer
    * @param flux is a {@code Flux} or not
    * @param call the client call
-   * @param mutex the mutex to unlock
+   * @param cond the lock to unlock
    */
-  private <T> void manageResponse(final ClientResponse res, final Class<T> clazz, final Consumer<T> onSuccess, final boolean flux, final ClientCall call, final Semaphore mutex) {
-    manageResponse(res, clazz,  HttpStatus.OK, onSuccess, flux, call, mutex);
+  private <T> void manageResponse(final ClientResponse res, final Class<T> clazz, final Consumer<T> onSuccess, final boolean flux, final ClientCall call, final Lock lock, final Condition cond) {
+    manageResponse(res, clazz, HttpStatus.OK, onSuccess, flux, call, lock, cond);
   }
 
   /**
@@ -130,9 +143,10 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
    * @param onSuccess the on success consumer
    * @param flux is a {@code Flux} or not
    * @param call the client call
-   * @param mutex the mutex to unlock
+   * @param cond the lock to unlock
    */
-  private <T> void manageResponse(final ClientResponse res, final Class<T> clazz, final HttpStatus expStatus, final Consumer<T> onSuccess, final boolean flux, final ClientCall call, final Semaphore mutex) {
+  private <T> void manageResponse(final ClientResponse res, final Class<T> clazz, final HttpStatus expStatus, final Consumer<T> onSuccess, final boolean flux, final ClientCall call, final Lock lock, final Condition cond) {
+    lock.lock();
     if(res.statusCode() == expStatus) {
       if(flux) {
         res.bodyToFlux(clazz).doOnNext(onSuccess::accept)
@@ -141,7 +155,8 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
           call.setErrMsg(e.getMessage());
         }).doFinally(t -> {
           call.setClientEnd(System.nanoTime());
-          mutex.release();
+          cond.signal();
+          lock.unlock();
         }).subscribe();
 
       } else {
@@ -151,7 +166,8 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
           call.setErrMsg(e.getMessage());
         }).doFinally(t -> {
           call.setClientEnd(System.nanoTime());
-          mutex.release();
+          cond.signal();
+          lock.unlock();
         }).subscribe();
       }
 
@@ -159,23 +175,11 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
       call.setOk(false);
       res.bodyToMono(String.class).doOnNext(err -> {
         call.setErrMsg("<" + res.statusCode() + "> " + err);
-      }).doFinally( t-> {
+      }).doFinally(t-> {
         call.setClientEnd(System.nanoTime());
-        mutex.release();
+        cond.signal();
+        lock.unlock();
       }).subscribe();
-    }
-  }
-
-  /**
-   * Lock mutex
-   *
-   * @param mutex the semaphore
-   */
-  private void mutexLock(final Semaphore mutex) {
-    try {
-      mutex.acquire();
-    } catch(final InterruptedException e) {
-      // Nothing to do
     }
   }
 
@@ -191,16 +195,18 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
     call.setRequestSeq(requestSeq);
     call.setOk(true);
     call.setProtocol("webflux");
-    final Semaphore mutex = new Semaphore(1);
-    mutexLock(mutex);
-    final Consumer<ClientResponse> okFunc = res -> manageResponse(res, String.class, HttpStatus.CREATED, str -> customer.setId(str), false, call, mutex);
-    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, mutex);
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+    lock.lock();
+    final Consumer<ClientResponse> okFunc = res -> manageResponse(res, String.class, HttpStatus.CREATED, str -> customer.setId(str), false, call, lock, cond);
+    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, lock, cond);
     call.setClientStart(System.nanoTime());
     client.mutate().build().post().accept(MediaType.TEXT_PLAIN).contentType(MediaType.APPLICATION_JSON_UTF8)
     .header("request_seq", Integer.toString(requestSeq)).header("mapper", mapper)
     .body(BodyInserters.fromObject(customer)).exchange()
     .doOnNext(okFunc).doOnError(koFunc).subscribe();
-    mutexLock(mutex);
+    cond.awaitUninterruptibly();
+    lock.unlock();
     return call;
   }
 
@@ -217,15 +223,17 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
     call.setOk(true);
     call.setProtocol("webflux");
     final ArrayList<Customer> customers = new ArrayList<>();
-    final Semaphore mutex = new Semaphore(1);
-    mutexLock(mutex);
-    final Consumer<ClientResponse> okFunc = res -> this.manageResponse(res, Customer.class, cust -> customers.add(cust), true, call, mutex);
-    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, mutex);
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+    lock.lock();
+    final Consumer<ClientResponse> okFunc = res -> this.manageResponse(res, Customer.class, cust -> customers.add(cust), true, call, lock, cond);
+    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, lock, cond);
     call.setClientStart(System.nanoTime());
     client.mutate().build().get().accept(MediaType.APPLICATION_STREAM_JSON)
     .header("request_seq", Integer.toString(requestSeq)).header("mapper", mapper)
     .exchange().doOnNext(okFunc).doOnError(koFunc).subscribe();
-    mutexLock(mutex);
+    cond.awaitUninterruptibly();
+    lock.unlock();
     if(customers.size() != this.customers.size()) {
       LOG.warn("Customers size should be " + this.customers.size() + " instead of " + customers.size());
     }
@@ -244,19 +252,21 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
     call.setRequestSeq(requestSeq);
     call.setOk(true);
     call.setProtocol("webflux");
-    final Semaphore mutex = new Semaphore(1);
-    mutexLock(mutex);
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+    lock.lock();
     final Consumer<ClientResponse> okFunc = res -> manageResponse(res, Customer.class, cust -> {
       if(!cust.getId().equals(customer.getId())) {
         LOG.warn("Customer should have " + customer.getId() + " instead of " + cust.getId());
       }
-    }, false, call, mutex);
-    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, mutex);
+    }, false, call, lock, cond);
+    final Consumer<Throwable> koFunc = e -> manageClientError(e, call, lock, cond);
     call.setClientStart(System.nanoTime());
     client.mutate().build().get().uri("/{id}", customer.getId()).accept(MediaType.APPLICATION_JSON_UTF8)
     .header("request_seq", Integer.toString(requestSeq)).header("mapper", mapper)
     .exchange().doOnNext(okFunc).doOnError(koFunc).subscribe();
-    mutexLock(mutex);
+    cond.awaitUninterruptibly();
+    lock.unlock();
     return call;
   }
 
@@ -297,8 +307,9 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
     }
     // Gather test suite informations -
 
-    final Semaphore mutex = new Semaphore(1);
-    mutexLock(mutex);
+    final Lock lock = new ReentrantLock();
+    final Condition cond = lock.newCondition();
+    lock.lock();
     // Insert test suite general informations +
     LOG.info("Register a new test suite");
     statsClient.mutate().build().post().contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -313,39 +324,42 @@ public class RESTWebfluxClient extends AbstractClientTestSuite<Customer,ClientCa
       }).doOnError(e -> {
         LOG.error("HTTP status code: {}, error: {}", res.statusCode(), e.getMessage(), e);
       }).doFinally(t -> {
-        mutex.release();
+        lock.lock();
+        cond.signal();
+        lock.unlock();
       }).subscribe();
     }).doOnError(e -> {
       LOG.error(e.getMessage(), e);
     }).subscribe();
-    mutexLock(mutex);
+    cond.awaitUninterruptibly();
     // Insert test suite general informations -
 
     if(suite.getId() != null) {
       // Insert calls +
       LOG.info("Add calls to testsuite {}", suite.getId());
-      mutex.release();
-      mutexLock(mutex);
       statsClient.mutate().build().post().uri("/{id}/calls", suite.getId()).contentType(MediaType.APPLICATION_STREAM_JSON)
       .body(BodyInserters.fromPublisher(Flux.fromIterable(calls), ClientCall.class))
       .exchange().doOnError(e -> {
         LOG.error("Error while inserting call: " + e.getMessage(), e);
       }).doFinally(t -> {
-        mutex.release();
+        lock.lock();
+        cond.signal();
+        lock.unlock();
       }).subscribe();
-      mutexLock(mutex);
+      cond.awaitUninterruptibly();
       // Insert calls -
     } else {
       LOG.error("Test suite Identifier is null");
     }
     // Delete calls cache +
     LOG.info("Purge statistics cache");
-    mutex.release();
-    mutexLock(mutex);
     statsClient.mutate().build().delete().exchange().doFinally(t -> {
-      mutex.release();
+      lock.lock();
+      cond.signal();
+      lock.unlock();
     }).subscribe();
-    mutexLock(mutex);
+    cond.awaitUninterruptibly();
+    lock.unlock();
     // Delete calls cache -
   }
   // Methods -
